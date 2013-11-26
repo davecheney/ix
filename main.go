f48d6a57226944dca47fbb8750669b69a87d17ee
Dave Cheney <dave@cheney.net>
2013-11-26 17:51:15 +1100

    Convert to martini-contrib/render

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"html/template"
-	"path/filepath"
+	"log"
 	"strconv"
 
 	"github.com/codegangsta/martini"
-	"github.com/kr/fs"
+	"github.com/codegangsta/martini-contrib/render"
 )
 
 var model = new(Model)
-var view *template.Template
 
 func atoi(s string) int {
 	i, err := strconv.Atoi(s)
@@ -20,90 +16,76 @@ func atoi(s string) int {
 	return i
 }
 
-func LoadTemplates() {
-	walker := fs.Walk("templates")
-	var files []string
-	for walker.Step() {
-		fi := walker.Stat()
-		if fi.IsDir() {
-			continue
-		}
-		files = append(files, filepath.Join("templates", fi.Name()))
-	}
-	fatal(walker.Err())
-	var err error
-	view, err = template.ParseFiles(files...)
-	fatal(err)
-}
-
-func View(name string, arg interface{}) (int, string) {
-	var w bytes.Buffer
-	if err := view.ExecuteTemplate(&w, name, arg); err != nil {
-		return 501, err.Error()
-	}
-	return 200, w.String()
-}
-
-func ShowIssue(params martini.Params) (int, string) {
+func ShowIssue(r render.Render, params martini.Params) {
 	id := atoi(params["id"])
 	issue, ok := model.FindIssueById(id)
 	if !ok {
-		return 404, fmt.Sprintf("Issue %d not found", id)
+		r.HTML(404, "error/404", nil)
+		return
 	}
-	return View("showissue", issue)
+	r.HTML(200, "showissue", issue)
 }
 
-func ShowTag(params martini.Params) (int, string) {
+func ShowTag(r render.Render, params martini.Params) {
 	name := params["name"]
 	issues := model.FindIssuesByTag(name)
 	if len(issues) == 0 {
-		return 404, fmt.Sprintf("Tag %q not found", name)
+		r.HTML(404, "error/404", name)
+		return
 	}
-	return View("showtag", struct {
+	r.HTML(200, "showtag", struct {
 		Name   string
 		Issues []*Issue
 	}{name, ById(issues)})
 }
 
-func ShowStatus(params martini.Params) (int, string) {
+func ShowStatus(r render.Render, params martini.Params) {
 	status := params["status"]
 	issues := model.FindIssuesByStatus(status)
 	if len(issues) == 0 {
-		return 404, fmt.Sprintf("Status %q not found", status)
+		r.HTML(404, "error/404", status)
+		return
 	}
-	return View("showstatus", struct {
-		Status  string
+	r.HTML(200, "showstatus", struct {
+		Status string
 		Issues []*Issue
 	}{status, ById(issues)})
 }
 
-func ShowTagAndStatus(params martini.Params) (int, string) {
+func ShowTagAndStatus(r render.Render, params martini.Params) {
 	name, status := params["name"], params["status"]
 	issues := model.FindIssuesByTagAndStatus(name, status)
 	if len(issues) == 0 {
-		return 404, fmt.Sprintf("Tag/Status %q/%q not found", name)
+		r.HTML(404, "error/404", name)
+		return
 	}
-	return View("showtag", struct {
+	r.HTML(200, "showtag", struct {
 		Name   string
 		Issues []*Issue
 	}{name, ById(issues)})
 }
 
-func ShowAllTags() (int,string) {
+func ShowAllTags(r render.Render) {
 	tags := model.FindTags()
-	return View("showtags", tags)
+	r.HTML(200, "showtags", struct{ Tags []string }{tags})
 }
 
-func Overview() (int, string) {
+func Overview(r render.Render) {
 	tags := model.FindTags()
-	return View("overview", struct{ Tags []string }{tags})
+	r.HTML(200, "overview", struct{ Tags []string }{tags})
 }
 
 func main() {
-	LoadTemplates()
-	go model.LoadIssues("issues")
+	go func() {
+		model.LoadIssues("issues")
+		log.Println("issues loaded")
+		//		model.LoadComments("comments")
+		//		log.Println("comments loaded")
+	}()
 
 	m := martini.Classic()
+	m.Use(render.Renderer("templates"))
+
 	m.Get("/issue/:id", ShowIssue)
 	m.Get("/tag/:name", ShowTag)
 	m.Get("/tag", ShowAllTags)
